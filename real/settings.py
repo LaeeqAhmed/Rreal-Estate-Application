3c0aef83b6f42ded9d9c1ccda83dcc8ea3b09eab
Laeeq Ahmed <laeeqahmed0300@gmail.com>
2021-04-12 23:46:24 +0500

    creating listings models

--- real/settings.py
+++ real/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
+    'realtors.app.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
