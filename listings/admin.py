2828eb0c5acab9a71630ec6a0b328cacbea4c615
Laeeq Ahmed <laeeqahmed0300@gmail.com>
2021-05-09 18:30:44 +0500

    working on admin module

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here. to manipulate with respect to the admin area
+
+from .models import Listing
+admin.site.register(Listing)
