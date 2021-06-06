2fa8a2cfab502254b0d458613ca315b2d927e67f
Laeeq Ahmed <laeeqahmed0300@gmail.com>
2021-06-06 19:44:22 +0500

    working of admin realtor module

--- realtors/admin.py
+++ realtors/admin.py
@@ -7,6 +7,8 @@ from .models import Realtor
 class RealtorAdmin(admin.ModelAdmin):
     list_display = ('id','name','email','hire_date')
     list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
 
 #calling
 admin.site.register(Realtor,RealtorAdmin)
