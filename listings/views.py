6108b3809305c6ba64ef1acc24ce9b0746a89e71
Laeeq Ahmed <laeeqahmed0300@gmail.com>
2021-06-12 02:51:01 +0500

    front-end listings updation

--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+#import models here
+from .models import Listing
 # Create your views here.
 def index(request):
-    return render(request, 'listings/listings.html')
+    #set variable to show the listings
+    listings = Listing.objects.all()   
+    context = {
+        'listings': listings
+    } 
+    return render(request, 'listings/listings.html',context)
 
 
 def listing(request):
