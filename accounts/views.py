e5e7717b8eec0fd0a315fa443ef942d41e0657d2
Laeeq Ahmed <laeeqahmed0300@gmail.com>
2021-08-06 04:02:18 +0500

    accounts app and respective templates working

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+# Create your accounts views here.
+
+def register(request):
+    return render(request, 'accounts/register.html')
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return redirect('index')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
--- real/urls.py
+++ real/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #just add the static files to show the media files in the explorer
